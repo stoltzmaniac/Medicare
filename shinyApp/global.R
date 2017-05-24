a6858110177599dd26acf004e10f876fef17c2a7
ginberg <ginberg@gmail.com>
2017-05-24 12:44:03 +0200

    Make the plot interactive with plotly

--- shinyApp/global.R
+++ shinyApp/global.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(scales)
 library(leaflet)
 library(DT)
+library(plotly)
 
 #Data from https://catalog.data.gov/dataset/healthcare-associated-infections-hospital-3ca5e
 #TheÂ Healthcare-Associated Infection (HAI)Â measures - provider data. These measures are developed byÂ Centers for Disease Control and Prevention (CDC)Â and collected through theÂ National Healthcare Safety Network (NHSN). They provide information on infections that occur while the patient is in the hospital. These infections can be related to devices, such as central lines and urinary catheters, or spread from patient to patient after contact with an infected person or surface. Many healthcare associated infections can be prevented when the hospitals useÂ CDC-recommended infection control steps.
